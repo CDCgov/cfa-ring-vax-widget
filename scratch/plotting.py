55a6e33a6c1e6a7e7e4b0b9999deaa8352b6c495
afmagee42 <rzg0@cdc.gov>
2024-12-23 12:48:58 -0800

    works

--- scratch/plotting.py
+++ scratch/plotting.py
@@ -27,31 +27,63 @@ stage_map = {
 }
 
 
-def get_end(infection, stage, stage_map):
-    if (
-        infection["detected"]
-        and infection["t_detected"] < infection[stage_map[stage]["end"]]
-    ):
-        return infection["t_detected"]
+def diamond(xc, yc, width, height, ppl):
+    half_x_l = np.linspace(xc - width / 2.0, xc, ppl).tolist()
+    half_x_r = np.linspace(xc, xc + width / 2.0, ppl).tolist()
+    x = np.array(
+        [*half_x_l[:-1], *half_x_r[:-1], *reversed(half_x_r[1:]), *reversed(half_x_l)]
+    )  # type: ignore
+
+    half_up_y = np.linspace(yc, yc + height / 2.0, ppl).tolist()
+    half_down_y = np.linspace(yc - height / 2.0, yc, ppl).tolist()
+    y = np.array(
+        [
+            *half_up_y[:-1],
+            *reversed(half_up_y[1:]),
+            *reversed(half_down_y[1:]),
+            *half_down_y,
+        ]
+    )  # type: ignore
+
+    return (
+        x,
+        y,
+    )
+
+
+def get_end(
+    id: str, sim: ringvax.Simulation, stage: str, stage_map: dict[str, dict[str, str]]
+):
+    if sim.get_person_property(id, "detected") and sim.get_person_property(
+        id, "t_detected"
+    ) < sim.get_person_property(id, stage_map[stage]["end"]):
+        return sim.get_person_property(id, "t_detected")
     else:
-        return infection[stage_map[stage]["end"]]
+        return sim.get_person_property(id, stage_map[stage]["end"])
 
 
-def mark_detection(ax, id, infections, plot_par):
-    infection = infections[id]
-    y_loc = plot_par["height"][id]
-    if infection["detected"]:
-        y = np.linspace(
-            y_loc - plot_par["history_thickness"] / 2.0,
-            y_loc + plot_par["history_thickness"] / 2.0,
+def mark_detection(ax, id, sim: ringvax.Simulation, plot_par):
+    infection = sim.infections[id]
+    if sim.get_person_property(id, "detected"):
+        y_loc = plot_par["height"][id]
+        x_loc = sim.get_person_property(id, "t_detected")
+        x_adj = (
+            0.5
+            * (max(plot_par["x_range"]) - min(plot_par["x_range"]))
+            / (max(plot_par["y_range"]) - min(plot_par["y_range"]))
+        )
+        x, y = diamond(
+            x_loc,
+            y_loc,
+            plot_par["history_thickness"] * x_adj,
+            plot_par["history_thickness"],
             plot_par["ppl"],
         )
-        x = np.array([infection["t_detected"]] * len(y))
-        ax.plot(x, y, color=plot_par["color"]["detection"][infection["detect_method"]])
+        ax.fill(x, y, color=plot_par["color"]["detection"][infection["detect_method"]])
 
 
-def mark_infections(ax, id, infections, plot_par):
-    infection = infections[id]
+def mark_infections(ax, id, sim: ringvax.Simulation, plot_par):
+    infection = sim.infections[id]
     y_loc = plot_par["height"][id]
     if infection["infection_times"].size > 0:
         for t in infection["infection_times"]:
@@ -64,13 +96,13 @@ def mark_infections(ax, id, infections, plot_par):
             ax.plot(x, y, color=plot_par["color"]["infection"])
 
 
-def draw_stages(ax, id, infections, plot_par):
-    infection = infections[id]
+def draw_stages(ax, id, sim: ringvax.Simulation, plot_par):
+    infection = sim.infections[id]
     y_loc = plot_par["height"][id]
     for stage in ["latent", "infectious"]:
         x = np.linspace(
             infection[stage_map[stage]["start"]],
-            get_end(infection, stage, stage_map),
+            get_end(id, sim, stage, stage_map),
             plot_par["ppl"],
         )
 
@@ -84,48 +116,41 @@ def draw_stages(ax, id, infections, plot_par):
         )
 
 
-def connect_child_infections(ax, id, infections, plot_par):
-    print(f"Connecting children of {id}")
-    infection = infections[id]
+def connect_child_infections(ax, id, sim: ringvax.Simulation, plot_par):
     y_parent = plot_par["height"][id]
-    infectees = infection["infectees"]
-    if infectees is not None and len(infectees) > 0:
-        times = infection["infection_times"]
-        infectees = infection["infectees"]
-        for neg_t, inf in sorted(zip(-times, infectees)):
+    times_infections = get_infection_time_tuples(id, sim)
+    if times_infections is not None:
+        for t, inf in times_infections:
             assert (
-                infections[inf]["t_exposed"] == -neg_t
-            ), f"Child {inf} reports infection at time {infections[inf]['t_exposed']} while parent reports time was {-neg_t}"
+                sim.infections[inf]["t_exposed"] == t
+            ), f"Child {inf} reports infection at time {sim.infections[inf]['t_exposed']} while parent reports time was {t}"
             y_child = plot_par["height"][inf]
             y = np.linspace(
                 y_child - plot_par["history_thickness"] / 2.0,
                 y_parent - plot_par["history_thickness"] / 2.0,
                 plot_par["ppl"],
             )
-            x = np.array([-neg_t] * len(y))
+            x = np.array([t] * len(y))
             ax.plot(x, y, color=plot_par["color"]["connection"])
 
 
-def get_descendants(sim: ringvax.Simulation, infection: str, offspring: list[str]):
+def get_descendants(id, sim: ringvax.Simulation, offspring: list[str]):
     """
     Get all actually-simulated infections caused by this infection, directly and indirectly.
     """
-    infectees = sim.get_person_property(infection, "infectees")
+    infectees = sim.get_person_property(id, "infectees")
     if infectees is not None and len(infectees) > 0:
         for inf in infectees:
             offspring.append(inf)
             get_descendants(sim, inf, offspring)
 
 
-def get_infection_time_tuples(sim: ringvax.Simulation, id: str):
+def get_infection_time_tuples(id: str, sim: ringvax.Simulation):
     infectees = sim.get_person_property(id, "infectees")
     if infectees is None or len(infectees) == 0:
         return None
 
-    times_infections = [
-        (sim.get_person_property(inf, "t_exposed"), inf) for inf in infectees
-    ]
-    return times_infections
+    return [(sim.get_person_property(inf, "t_exposed"), inf) for inf in infectees]
 
 
 def order_descendants(sim: ringvax.Simulation):
@@ -134,27 +159,29 @@ def order_descendants(sim: ringvax.Simulation):
     #    1. Each individuals' most recent offspring is plotted at the next height
     #    2. All subsequent offspring heights are adjusted for the number of infections produced by more-recent infections
     order = ["0"]
-    _order_descendants(sim, "0", order)
+    _order_descendants("0", sim, order)
     return order
 
 
-def _order_descendants(sim: ringvax.Simulation, id: str, order: list[str]):
+def _order_descendants(id: str, sim: ringvax.Simulation, order: list[str]):
     assert id in order, f"Cannot append infection {id} to ordering list {order}"
-    times_infections = get_infection_time_tuples(sim, id)
+    times_infections = get_infection_time_tuples(id, sim)
+    print(f"Adding {id} to {order}\n")
+    print(f"inf t tups = {times_infections}")
     if times_infections is not None:
         for _, inf in times_infections:
             order.insert(order.index(id) + 1, inf)
-            _order_descendants(sim, inf, order)
+            _order_descendants(inf, sim, order)
 
 
 plot_order = order_descendants(s)
 
 plot_par = {
     "color": {
-        "latent": "#c7dcdd",
-        "infectious": "#cf4828",
-        "infection": "#000000",
-        "connection": "#bbbbbb",
+        "latent": "#888888",
+        "infectious": "#c7dcdd",
+        "infection": "#888888",
+        "connection": "#888888",
         "detection": {
             "active": "#068482",
             "passive": "#f78f47",
@@ -170,6 +197,11 @@ plot_par = {
     "height": {
         inf: len(plot_order) - 1.0 * height for height, inf in enumerate(plot_order)
     },
+    "x_range": [
+        0.0,
+        max(get_end(id, s, "infectious", stage_map) for id in s.infections.keys()),
+    ],
+    "y_range": [0.0, len(s.infections)],
 }
 
 # plot
@@ -177,12 +209,12 @@ fig, ax = plt.subplots()
 
 
 for inf in s.infections.keys():
-    draw_stages(ax, inf, s.infections, plot_par)
+    draw_stages(ax, inf, s, plot_par)
 
-    mark_detection(ax, inf, s.infections, plot_par)
+    mark_detection(ax, inf, s, plot_par)
 
-    mark_infections(ax, inf, s.infections, plot_par)
+    mark_infections(ax, inf, s, plot_par)
 
-    connect_child_infections(ax, inf, s.infections, plot_par)
+    connect_child_infections(ax, inf, s, plot_par)
 
 plt.show()
